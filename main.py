59e40b41ef0b964d101841dd615d6d77a21933f3
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-10-07 20:14:31 -0400

    feat: add Vector store and ready to retrieve data

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # WebBaseLoader: To load documents from a URL
 # load_dotenv: To load environment variables (like API keys) from a .env file
 # RecursiveCharacterTextSplitter: To split large documents into smaller chunks
+# OpenAIEmbeddings: To create embedding vectors for each chunk of data
 from langchain_community.document_loaders import WebBaseLoader
 from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 import os
 
 load_dotenv()
@@ -31,8 +34,32 @@ text_splitter = RecursiveCharacterTextSplitter(
 # Execute the split operation
 splits = text_splitter.split_documents(docs)
 
-# --- 3. INSPECT THE RESULTS ---
+# --- 3. EMBEDDING VECTOR FOR EACH CHUNK ---
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    dimensions=1024
+)
 
-print(f"Split the document into {len(splits)} chunks")
-print(splits[1].page_content)
+# Initialize ChromaDB
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+print("Successfully created vector store.")
+
+# INSPECT THE RESULTS ---
+
+# print(f"Split the document into {len(splits)} chunks")
+# print(splits[1].page_content)
+
+# Create a sample query to test the vector store
+query = "how to use WebBaseLoader"
+
+# Perform a similarity search. 
+retrieved_docs = vectorstore.similarity_search(query)
+
+# Print the number of documents retrieved (by default, it's 4)
+print(f"\nRetrieved {len(retrieved_docs)} documents for the query: '{query}'")
+
+# Print the content of the most relevant document chunk
+print("\n--- Most Relevant Chunk ---")
+print(retrieved_docs[0].page_content)
 
